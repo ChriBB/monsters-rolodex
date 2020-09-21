7d2c8ab4b4ecc01231d9d0d333d998f0ddea5e0a
Your Name <your@email.com>
2020-09-21 13:41:59 +0200

    Updates

---
+++ precache-manifest.50dd93aaacc2e6a4e0f3d074e3c12e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd8f0d45ec9cb180707d20d171320ea",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1fb7ece32574899ecc1c",
+    "url": "/monsters-rolodex/static/css/main.f602f80f.chunk.css"
+  },
+  {
+    "revision": "9e0c054ab49306d0a57a",
+    "url": "/monsters-rolodex/static/js/2.9d1cbf2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9d1cbf2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb7ece32574899ecc1c",
+    "url": "/monsters-rolodex/static/js/main.f1c8e6cc.chunk.js"
+  },
+  {
+    "revision": "837c1268ee6263019fa1",
+    "url": "/monsters-rolodex/static/js/runtime-main.f6e424f3.js"
+  }
+]);
